4730a860b8073de49fc490bba39f7487e31b4c67
DioenDJS <negociospacheti@gmail.com>
2026-03-01 19:13:08 -0300

    ✨ Implements apscheduler

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def test_schedule():
+    print("test..")
 
 def main() -> None:
-    print("Running main function")
+    print("Starting the scheduler...")
+
+    scheduler = BlockingScheduler()
+    scheduler.add_job(test_schedule, 'interval', seconds=10)
+    scheduler.start()
 
 if __name__ == "__main__":
     main()
