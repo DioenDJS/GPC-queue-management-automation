d7d16c4cf7c6d309a54cca51caddc765d4a9146e
DioenDJS <negociospacheti@gmail.com>
2026-03-01 23:33:42 -0300

    🎨 Create file settings

---
+++ common/config/settings.py
@@ -0,0 +1,7 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    base_url_pubsub: str = "https://pubsub.googleapis.com/v1/projects/integracaon8n-486515/"
+    scopes_pubsub: str = "https://www.googleapis.com/auth/pubsub"
+
+settings = Settings()
