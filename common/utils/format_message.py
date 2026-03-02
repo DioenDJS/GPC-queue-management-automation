4d68ef547a17dd739a89a1a1a938fe39e51d95e2
DioenDJS <negociospacheti@gmail.com>
2026-03-02 19:28:33 -0300

    ♻️ Refactor formatting msg dlq

--- common/utils/format_message.py
+++ common/utils/format_message.py
@@ -1,32 +1,29 @@
 import base64
 import json
 
+from common.schemas import ContentDlqSchema
+
 list_dlqs = []
-content_dlqs = {
-    "dlq": "",
-    "ackId": "",
-    "data": "",
-    "publishTime": "",
-    "data_decoded": "",
-    "topic": "",
-}
 
 
 def formatting(msg):
     for item in msg:
         for key, value in item.items():
-            content_dlqs["dlq"] = key
-            content_dlqs["topic"] = value.get("topic", "")
+            topic = value.get("topic", "")
             for message in value.get("receivedMessages", []):
-                content_dlqs["ackId"] = message.get("ackId")
-                content_dlqs["data"] = message.get("message", {}).get("data")
-                content_dlqs["publishTime"] = message.get("message", {}).get(
-                    "publishTime"
+                content_dlq = ContentDlqSchema(
+                    dlq=key,
+                    topic=topic,
+                    ackId=message.get("ackId"),
+                    data=message.get("message", {}).get("data"),
+                    publish_time=message.get("message", {}).get(
+                        "publishTime"
+                    ),
+                    data_decoded=decode_data(
+                        message.get("message", {}).get("data", "")
+                    )
                 )
-                content_dlqs["data_decoded"] = decode_data(
-                    message.get("message", {}).get("data", "")
-                )
-                list_dlqs.append(content_dlqs.copy())
+                list_dlqs.append(content_dlq)
 
     return list_dlqs
 
