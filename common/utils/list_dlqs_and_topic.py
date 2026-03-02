654ae7239bc0b1c09540f589e6682e26cb4192cd
DioenDJS <negociospacheti@gmail.com>
2026-03-01 23:32:48 -0300

    🎨 Create file list de subscription dlqs and topic

---
+++ common/utils/list_dlqs_and_topic.py
@@ -0,0 +1,27 @@
+def get_list_dlqs_and_topic():
+    return [
+        {
+            "subscription": "subscription-fila-um-dlq",
+            "topic": "date-me-topic-fila-um-queue",
+        },
+        {
+            "subscription": "subscription-fila-dois-dlq",
+            "topic": "date-me-topic-fila-dois-queue",
+        },
+        {
+            "subscription": "subscription-fila-tres-dlq",
+            "topic": "date-me-topic-fila-tres-queue",
+        },
+        {
+            "subscription": "subscription-fila-quatro-dlq",
+            "topic": "date-me-topic-fila-quatro-queue",
+        },
+        {
+            "subscription": "subscription-reporting-reported-posts-dlq",
+            "topic": "date-me-topic-reporting-reported-posts-queue",
+        },
+        {
+            "subscription": "subscription-reporting-reported-posts-resolved-dlq",
+            "topic": "date-me-topic-reporting-reported-comments-queue",
+        }
+    ]
